371a5b7e84b2964dbb7966553c0448bd115975af
Abhinay Singh Negi <negia394@gmail.com>
2024-05-14 17:15:23 +0530

    Started working on javascript

---
+++ script.js
@@ -0,0 +1,6 @@
+const studentName = document.getElementsByName('studentName');
+const submitBtn = document.getElementsByName('registerBtn');
+console.log(submitBtn);
+submitBtn.onclick = function () {
+    console.log("hello");
+}
