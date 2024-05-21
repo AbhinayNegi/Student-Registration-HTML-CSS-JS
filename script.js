c8720f5552b7e108b86dc287f47f0f74ffbc3f66
Abhinay Singh Negi <negia394@gmail.com>
2024-05-21 07:46:47 +0530

    Form submission event handling. Added check for empty values

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-const studentName = document.getElementsByName('studentName');
-const submitBtn = document.getElementsByName('registerBtn');
-console.log(submitBtn);
-submitBtn.onclick = function () {
-    console.log("hello");
+const form = document.getElementById("form");
+
+form.addEventListener("submit", formSubmission);
+
+function formSubmission(event) {
+    event.preventDefault(); // Prevent default form submission
+
+    // Get form values and trim the spaces so that we can further check if user has submited form with empty spaces only
+    let name = document.getElementById("name").value.trim();
+    let studentId = document.getElementById("studentId").value.trim();
+    let email = document.getElementById("email").value.trim();
+    let number = document.getElementById("number").value.trim();
+
+    // Checking if the user has submitted empty values
+    if (name === "" || studentId === "" || email === "" || number === "") {
+        alert("Please don't leave any field empty.");
+        return;
+    }
+    console.log(name, studentId, email, number);
+
+    document.getElementById("form").reset();
 }
